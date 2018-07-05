938d46b82fc85eb386990f0b497b1c1c1e76b820
Bruce Becker <brucellino@gmail.com>
2018-07-05 20:56:14 +0200

    Add initial QC_DIST test

---
+++ molecule/default/tests/test_QC_DIST.py
@@ -0,0 +1,23 @@
+import os
+import testinfra.utils.ansible_runner
+import pytest
+# See http://egi-qc.github.io/#INSTALLATION
+# Packages must install without issues in a machine configured without any
+# external repositories (valid repositories are the standard OS repo, UMD repo
+# and EPEL repo for RH based distros)
+# Packages must follow the OS policies (name of packages, use of filesystem
+# hierarchy, init scripts, ...). For any detected issue, open a ticket.
+# Packages must be signed (or the repository where they are fetched from is
+# signed for Debian-based distros)
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+  os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+def packages():
+    listfile = open("list.txt", "r")
+    packages = listfile.read().splitlines()
+    return packages
+
+
+@pytest.mark.parametrize("pkg", packages())
+def test_packages(host, pkg):
+    assert host.package(pkg).is_installed
