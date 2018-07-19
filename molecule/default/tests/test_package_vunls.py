ec2f344ff7a9d527e63d7898b2f75257c7907209
Bruce Becker <brucellino@gmail.com>
2018-07-19 22:35:58 +0200

    Tag image in Travis build

--- molecule/default/tests/test_package_vunls.py
+++ molecule/default/tests/test_package_vunls.py
@@ -3,6 +3,10 @@ import testinfra.utils.ansible_runner
 import pytest
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+# The vulnerability scanner on Quay gives us intelligence on which
+# vulnerabilities are exposed by packages included in these images.
+# We therefore keep track of those and test to see whether the installed
+# version is greater than the one reported as fixing the vulnerability
 
 
 @pytest.mark.parametrize('name,version', [
