6ab5ca37c167129c3e605e55481c1997614de16f
Bruce Becker <brucellino@gmail.com>
2018-10-03 07:09:29 +0200

    Fix linting and change test names for OS

--- molecule/default/tests/test_package_vunls.py
+++ molecule/default/tests/test_package_vunls.py
@@ -12,7 +12,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 # Watch the CVEs raised at https://quay.io/repository/egi/ui?tab=tags
 
 
-@pytest.mark.parametrize('name,version', [
+@pytest.mark.parametrize_centos7('name,version', [
   ("gnupg2", "2.0.22-5.el7_5"),
   ("python", "2.7.5-69.el7_5"),
   ("python-libs", "2.7.5-69.el7_5"),
@@ -25,3 +25,18 @@ def test_vulnerable_packages(host, name, version):
     if (LooseVersion(host.system_info.release) >= '7.0.0'):
         p = host.package(name)
         assert p.release >= version
+
+
+# @pytest.mark.parametrize('name,version', [
+#   ("gnupg2", "2.0.22-5.el7_5"),
+#   ("python", "2.7.5-69.el7_5"),
+#   ("python-libs", "2.7.5-69.el7_5"),
+#   ("bind-license", "61.el7_5.1"),
+#   ("yum-utils", "1.1.31-46.el7_5"),
+#   ("yum-plugin-ovl", "1.1.31-46.el7_5"),
+#   ("yum-plugin-priorities", "1.1.31-46.el7_5"),
+#   ("yum-plugin-fastestmirror", "1.31-45.el7")])
+# def test_vulnerable_packages_centos6(host, name, version):
+#     if (LooseVersion(host.system_info.release) < '7.0.0'):
+#         p = host.package(name)
+#         assert p.release >= version
