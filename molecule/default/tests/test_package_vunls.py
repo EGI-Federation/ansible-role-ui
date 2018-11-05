9bb35b1016604bef856659950af1284d140de3d7
Bruce Becker <brucellino@gmail.com>
2018-11-05 15:51:48 +0100

    Add tests for some new CVE's
    vulnerability tests added:
    
      1. [RHSA-2018:2942](https://access.redhat.com/errata/RHSA-2018:2942)
      1. [RHSA-2018:2768](https://access.redhat.com/errata/RHSA-2018:2768)
      1. [RHSA-2018:3041](https://access.redhat.com/errata/RHSA-2018:3041)

--- molecule/default/tests/test_package_vunls.py
+++ molecule/default/tests/test_package_vunls.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 import os
 import testinfra.utils.ansible_runner
 import pytest
@@ -12,18 +13,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 # Watch the CVEs raised at https://quay.io/repository/egi/ui?tab=tags
 
 
-@pytest.mark.parametrize('name,version', [
-  ("gnupg2", "2.0.22-5.el7_5"),
-  ("python", "2.7.5-69.el7_5"),
-  ("python-libs", "2.7.5-69.el7_5"),
-  ("yum-utils", "1.1.31-46.el7_5"),
-  ("yum-plugin-ovl", "1.1.31-46.el7_5"),
-  ("yum-plugin-priorities", "1.1.31-46.el7_5"),
-  ("yum-plugin-fastestmirror", "1.31-45.el7")])
-def test_vulnerable_packages_centos7(host, name, version):
+@pytest.mark.parametrize('name,version,release', [
+  ('gnupg2', '2.0.22', '5.el7_5'),
+  ('java-1.8.0-openjdk', '1.8.0.191', '0.el7_5'),
+  ('nss-tools', '3.36.0', '7.el7_5'),
+  ('nss', '3.36.0', '7.el7_5'),
+  ('nss-sysinit', '3.36.0', '7.el7_5'),
+  ('python', '2.7.5', '69.el7_5'),
+  ('python-libs', '2.7.5', '69.el7_5'),
+  ('yum-utils', '1.1.31', '46.el7_5'),
+  ('yum-plugin-ovl', '1.1.31', '46.el7_5'),
+  ('yum-plugin-priorities', '1.1.31', '46.el7_5'),
+  ('yum-plugin-fastestmirror', '1.1.31', '46.el7_5')])
+def test_vulnerable_packages_centos7(host, name, version, release):
     if (LooseVersion(host.system_info.release) >= '7.0.0'):
         p = host.package(name)
-        assert p.release >= version
+        assert p.release.startswith(release)
+        assert p.version >= version
 
 
 # @pytest.mark.parametrize('name,version', [
